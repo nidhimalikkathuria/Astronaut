f7548a671b7e06d3056de72690e5bf3e7dc47816
Nidhi Malik Kathuria <74198624+nidhimalikkathuria@users.noreply.github.com>
2021-08-11 19:59:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+
+var bg,sleep, brush, gym, eat, bath, move;
+var astronaut;
+
+function preload(){
+  bg= loadImage("images/iss.png");
+  sleep = loadAnimation("images/sleep.png");
+  brush = loadAnimation("images/brush.png");
+  gym = loadAnimation( "images/gym1.png","images/gym2.png");
+}
+
+function setup() {
+  createCanvas(600, 500);
+  astronaut = createSprite(300, 230);
+  astronaut.addAnimation("sleeping",sleep);
+  astronaut.scale = 0.1;
+  
+}
+
+function draw() {
+  background(bg);
+  drawSprites();
+
+  textSize(20);
+  fill("white");
+  text("Instructions :", 20,35);
+  textSize(15);
+  text("Up Arrow = Brushing ", 20,55);
+  text("Down Arrow = Gyming", 20,70);
+  edges = createEdgeSprites();
+  astronaut.bounceOff(edges);
+  
+  
+  if(keyDown("UP_ARROW")){
+  astronaut.addAnimation("brushing", brush);
+  astronaut.changeAnimation("brushing");
+  astronaut.y = 350;
+  astronaut.velocityX = 0;
+  astronaut.velocityY = 0;
+
+    
+  }
+  
+  if(keyDown("DOWN_ARROW")){
+    astronaut.addAnimation("gymming", gym);
+    astronaut.changeAnimation("gymming");
+    astronaut.y = 350;
+    astronaut.velocityX = 0;
+  astronaut.velocityY = 0;
+    
+  }
+  
+  if(keyDown("LEFT_ARROW")){
+    
+  }
+  
+  if(keyDown("RIGHT_ARROW")){
+    
+  }
+
+  if(keyDown("m")){
+    
+  }
+
+}
